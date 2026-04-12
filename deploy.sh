9c828f91ce5ca7487d738b679725746b71b59240
only-hell <dnslodygin20032005@gmail.com>
2026-04-12 22:54:24 +0400

    feat: ultimate hybrid ci/cd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export IMAGE_TAG=${1:-lab4-clean}
-echo "Deploying image with tag: $IMAGE_TAG"
-docker compose down
+export IMAGE_TAG=${1:-latest}
+docker compose pull
 docker compose up -d
+# Даем базе проснуться и создаем таблицу пользователей (как мы делали раньше)
+sleep 15
+docker compose exec -T app python3 -c 'from app.utils.mysql_storage import MySQLStorage; db_config = {"host": "db", "user": "user", "password": "password", "database": "reminders"}; storage = MySQLStorage("system", db_config); storage.cursor.execute("INSERT IGNORE INTO users (username, password) VALUES (\"sa3000udp\", \"password\")"); storage.conn.commit()'
